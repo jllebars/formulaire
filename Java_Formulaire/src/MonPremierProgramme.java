b1b38ab6e7271be1c12a6beb22b6878783316fdd
JL Le Bars <jllebars@cefim-formation.net>
2014-06-12 16:46:48 +0200

    Ajout d'une nouvelle instruction dans MonPremierProjet Et création de la classe Personne.

--- Java_Formulaire/src/MonPremierProgramme.java
+++ Java_Formulaire/src/MonPremierProgramme.java
@@ -4,6 +4,7 @@ public class MonPremierProgramme {
 	public static void main(String[] args) {
 		// TODO Stub de la méthode généré automatiquement
 		System.out.println("Bonjour");
+		System.out.println(", le monde.");
 
 	}
 
